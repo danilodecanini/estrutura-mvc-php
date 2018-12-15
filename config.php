f2add60d8f4a16e53741eb7abf67589d39262c47
Danilo de Canini <danilodecanini@gmail.com>
2018-12-15 18:05:15 -0200

    Envio da Estrutura para o Git

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+require 'environment.php';
+
+$config = array();
+if(ENVIRONMENT == 'development') {
+	define("BASE_URL", "http://localhost/mvc_routers/");
+	$config['dbname'] = 'estrutura_mvc';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = 'root';
+} else {
+	define("BASE_URL", "http://meusite.com.br/");
+	$config['dbname'] = 'estrutura_mvc';
+	$config['host'] = 'localhost';
+	$config['dbuser'] = 'root';
+	$config['dbpass'] = 'root';
+}
+
+global $db;
+try {
+	$db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+} catch(PDOException $e) {
+	echo "ERRO: ".$e->getMessage();
+	exit;
+}
---
+++ controllers/noticiaController.php
@@ -0,0 +1,16 @@
+<?php
+class noticiaController extends controller {
+
+	public function index() {
+		
+	}
+
+	public function abrir($id) {
+		echo "ID da noticia: ".$id;
+	}
+
+	public function abrirtitulo($titulo) {
+		echo "TITULO: ".$titulo;
+	}
+
+}
